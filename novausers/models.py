e6ef774f36bedba038184c6a155ef258a4c44816
D4RK91GHT <rahulmajumdar400@gmail.com>
2024-04-16 01:41:29 +0530

    novausers app configured

--- novausers/models.py
+++ novausers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
 # Create your models here.
+class Users(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=120)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    # def __str__(self):
+    #     return self.email
