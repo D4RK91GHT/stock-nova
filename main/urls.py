af9a404fddb0d9f1f9570b3d156cbf1c8ca57b8f
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-05-04 13:33:39 +0530

    urls updated

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from novauser.views import register_view, login_view
+
 
 
 urlpatterns = [
-    # path('register/', views.register, name="tickers"),
-    # path('login/', views.userLogin, name="tickers"),
+    path('register/', register_view, name="tickers"),
+    path('login/', login_view, name="tickers"),
     path('tickers/', views.tickerList, name="tickers"),
     path('showdata/', views.showdata, name='showdata'),
     path('cstoken/', views.get_csrf_token, name='showdata'),
